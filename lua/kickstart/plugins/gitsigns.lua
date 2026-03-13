645a8a66b6730700482be0cdccd5edbba405cefe
Joel Shady <57103965+Quellens@users.noreply.github.com>
2026-03-13 22:57:58 +0100

    refactor hd keymap, change visualisation

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -11,11 +11,16 @@ return {
     ---@type Gitsigns.Config
     ---@diagnostic disable-next-line: missing-fields
     opts = {
+      signs = {
+        add = { text = '┃', show_count = false },
+        change = { text = '┃', show_count = false },
+      },
+      current_line_blame = true,
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
-
+        local g = {}
         local function map(mode, l, r, opts)
-          opts = opts or {}
+          opts = opts or g
           opts.buffer = bufnr
           vim.keymap.set(mode, l, r, opts)
         end
@@ -48,8 +53,15 @@ return {
         map('n', '<leader>hu', gitsigns.stage_hunk, { desc = 'git [u]ndo stage hunk' })
         map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
         map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
-        map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
-        map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
+        map('n', '<leader>hd', function()
+          if vim.wo.diff then
+            vim.cmd 'q'
+            vim.cmd 'BufferClose'
+            vim.cmd 'execute "normal! \\<C-o>"'
+          else
+            gitsigns.diffthis()
+          end
+        end, { desc = 'git [d]iff against index' })
         map('n', '<leader>hD', function() gitsigns.diffthis '@' end, { desc = 'git [D]iff against last commit' })
         -- Toggles
         map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
