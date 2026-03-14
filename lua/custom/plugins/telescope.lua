964fe7007de893c9584a2d62a22ba7e91df05329
Joel Shady <57103965+Quellens@users.noreply.github.com>
2026-03-14 20:52:47 +0100

    move telescope to plugins, removing undotree

---
+++ lua/custom/plugins/telescope.lua
@@ -0,0 +1,122 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  enabled = true,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = function() return vim.fn.executable 'make' == 1 end },
+    'nvim-tree/nvim-web-devicons',
+    'folke/todo-comments.nvim',
+    'debugloop/telescope-undo.nvim',
+    { 'nvim-telescope/telescope-ui-select.nvim' },
+  },
+  config = function()
+    local telescope = require 'telescope'
+    pcall(telescope.load_extension, 'fzf')
+    pcall(telescope.load_extension, 'ui-select')
+    -- local actions = require 'telescope.actions'
+    -- local transform_mod = require('telescope.actions.mt').transform_mod
+
+    -- local trouble = require 'trouble'
+    -- local trouble_telescope = require 'trouble.sources.telescope'
+
+    -- or create your custom action
+    -- local custom_actions = transform_mod {
+    --   open_trouble_qflist = function(prompt_bufnr) trouble.toggle 'quickfix' end,
+    -- }
+
+    telescope.setup {
+      -- defaults = {
+      --   path_display = { 'smart' },
+      --   mappings = {
+      --     i = {
+      --       ['<C-k>'] = actions.move_selection_previous, -- move to prev result
+      --       ['<C-j>'] = actions.move_selection_next, -- move to next result
+      --       ['<C-q>'] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+      --       ['<C-t>'] = trouble_telescope.open,
+      --     },
+      --   },
+      -- },
+      extensions = {
+        ['ui-select'] = { require('telescope.themes').get_dropdown() },
+        undo = {
+          -- telescope-undo.nvim config, see below
+        },
+      },
+    }
+
+    telescope.load_extension 'fzf'
+    telescope.load_extension 'undo'
+    -- set keymaps
+    local builtin = require 'telescope.builtin'
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
+    vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [K]eymaps' })
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+    vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[F]ind [S]elect Telescope' })
+    vim.keymap.set({ 'n', 'v' }, '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
+    vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
+    vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
+    vim.keymap.set('n', '<leader>fc', builtin.commands, { desc = '[F]ind [C]ommands' })
+    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>u', '<cmd>Telescope undo<cr>')
+    -- LSP
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('telescope-lsp-attach', { clear = true }),
+      callback = function(event)
+        local buf = event.buf
+
+        -- Find references for the word under your cursor.
+        vim.keymap.set('n', 'grr', builtin.lsp_references, { buffer = buf, desc = '[G]oto [R]eferences' })
+
+        -- Jump to the implementation of the word under your cursor.
+        -- Useful when your language has ways of declaring types without an actual implementation.
+        vim.keymap.set('n', 'gri', builtin.lsp_implementations, { buffer = buf, desc = '[G]oto [I]mplementation' })
+
+        -- Jump to the definition of the word under your cursor.
+        -- This is where a variable was first declared, or where a function is defined, etc.
+        -- To jump back, press <C-t>.
+        vim.keymap.set('n', 'grd', builtin.lsp_definitions, { buffer = buf, desc = '[G]oto [D]efinition' })
+
+        -- Fuzzy find all the symbols in your current document.
+        -- Symbols are things like variables, functions, types, etc.
+        vim.keymap.set('n', 'gO', builtin.lsp_document_symbols, { buffer = buf, desc = 'Open Document Symbols' })
+
+        -- Fuzzy find all the symbols in your current workspace.
+        -- Similar to document symbols, except searches over your entire project.
+        vim.keymap.set('n', 'gW', builtin.lsp_dynamic_workspace_symbols, { buffer = buf, desc = 'Open Workspace Symbols' })
+
+        -- Jump to the type of the word under your cursor.
+        -- Useful when you're not sure what type a variable is and you want to see
+        -- the definition of its *type*, not where it was *defined*.
+        vim.keymap.set('n', 'grt', builtin.lsp_type_definitions, { buffer = buf, desc = '[G]oto [T]ype Definition' })
+      end,
+    })
+
+    -- Override default behavior and theme when searching
+    vim.keymap.set('n', '<leader>/', function()
+      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+      })
+    end, { desc = '[/] Fuzzily search in current buffer' })
+
+    -- It's also possible to pass additional configuration options.
+    --  See `:help telescope.builtin.live_grep()` for information about particular keys
+    vim.keymap.set(
+      'n',
+      '<leader>f/',
+      function()
+        builtin.live_grep {
+          grep_open_files = true,
+          prompt_title = 'Live Grep in Open Files',
+        }
+      end,
+      { desc = '[F]ind [/] in Open Files' }
+    )
+
+    -- Shortcut for searching your Neovim configuration files
+    vim.keymap.set('n', '<leader>fn', function() builtin.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = '[F]ind [N]eovim files' })
+  end,
+}
