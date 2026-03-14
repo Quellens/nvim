48a338d957eccc4fae26d1e0d27f16ffff7f0185
Joel Shady <57103965+Quellens@users.noreply.github.com>
2026-03-14 20:55:15 +0100

    removing lsp logs

--- init.lua
+++ init.lua
@@ -270,7 +270,7 @@ require('lazy').setup({
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
-      { 'j-hui/fidget.nvim', opts = {} },
+      -- { 'j-hui/fidget.nvim', opts = {} },
     },
     config = function()
       -- Brief aside: **What is LSP?**
