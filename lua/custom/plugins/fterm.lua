97b816e06f910ba243a0ba47704917376ef29ca0
Joel Shady <57103965+Quellens@users.noreply.github.com>
2026-03-27 17:15:08 +0100

    change fterm settings

--- lua/custom/plugins/fterm.lua
+++ lua/custom/plugins/fterm.lua
@@ -1,6 +1,13 @@
 return {
   'numToStr/FTerm.nvim',
   config = function()
+    require('FTerm').setup {
+      border = 'double',
+      dimensions = {
+        height = 0.8,
+        width = 0.8,
+      },
+    }
     vim.keymap.set('n', '<A-i>', '<CMD>lua require("FTerm").toggle()<CR>')
     vim.keymap.set('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
   end,
