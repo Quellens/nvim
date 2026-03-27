92877d4d0d3b58348f34136af4e4b87315a0ddbc
Joel Shady <57103965+Quellens@users.noreply.github.com>
2026-03-27 20:03:52 +0100

    adding autotag

---
+++ lua/custom/plugins/ts-autotag.lua
@@ -0,0 +1,9 @@
+return {
+  'windwp/nvim-ts-autotag',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  opts = {
+    enable_close = true,
+    enable_rename = true,
+    enable_close_on_slash = false,
+  },
+}
