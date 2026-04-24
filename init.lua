b5095b90b4117e6c22ee60a480b5c623c1f1f874
Joel Shady <57103965+Quellens@users.noreply.github.com>
2026-04-24 22:55:26 +0200

    fix formatter

--- init.lua
+++ init.lua
@@ -475,7 +475,7 @@ require('lazy').setup({
           return nil
         else
           return {
-            timeout_ms = 500,
+            timeout_ms = 2000,
             lsp_format = 'fallback',
           }
         end
@@ -486,7 +486,10 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        javascript = { 'prettier', stop_after_first = true },
       },
     },
   },
