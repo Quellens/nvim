7d48807cfb596ff82a60b41afb4f9a067080da23
Joel Shady <joelseckershady@gmail.com>
2026-06-09 00:03:18 +0200

    change keymap for git-conflict

--- lua/custom/plugins/git-conflict.lua
+++ lua/custom/plugins/git-conflict.lua
@@ -1,5 +1,11 @@
 return {
   'akinsho/git-conflict.nvim',
   version = '*',
-  config = true,
+  -- config = true,
+  config = function()
+    require('git-conflict').setup {}
+
+    vim.keymap.set('n', 'cc', '<Plug>(git-conflict-ours)')
+    vim.keymap.set('n', 'ci', '<Plug>(git-conflict-theirs)')
+  end,
 }
