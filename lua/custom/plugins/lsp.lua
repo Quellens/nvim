9e53cacbe96387ff2fe5a4f07f163be078e4ab25
Joel Shady <joelseckershady@gmail.com>
2026-08-09 13:14:57 +0200

    in strudel is typescript disabled

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
   {
     'pmizio/typescript-tools.nvim',
-    enabled = true,
+    enabled = false,
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
     opts = function()
       return {
